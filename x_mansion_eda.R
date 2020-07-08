3439b1ec286c866ae7b2488d3fce03e0fd268341
Travis.Knoche <travvywavvy8@gmail.com>
2020-07-08 10:30:21 -0600

    added

--- x_mansion_eda.R
+++ x_mansion_eda.R
@@ -30,7 +30,45 @@ c %>%
   glm(any_unc~., family = "binomial", data = .) %>% 
   broom::tidy() -> tidy_coefs
 
+# no significant coefs
 tidy_coefs %>% 
   filter(p.value <= .05)
 
-###### code for repo ml_projects is at https://bitbucket.polarisalpha.com/projects/RP/repos/ml_projects/browse
+###########--------- tidymodels ---------############
+require(tidymodels)
+
+comic_bechdel <- dat$comic_bechdel %>% 
+  mutate(writer = fct_lump(writer, 4)) %>% 
+  mutate(series = as.factor(series)) %>% 
+  select(-artist) %>% 
+  mutate(cover_artist = fct_lump(cover_artist, 6)) %>% 
+  mutate(pass_bechdel = as.factor(pass_bechdel)) %>% 
+  select(-page_number) %>% 
+  select(-notes)
+
+pca_rec <- recipe(~., data = comic_bechdel) %>% 
+  step_pca(all_predictors())
+
+prepped <- prep(pca_rec)
+
+juice(pca_rec)
+
+#######
+
+dat <- comic_bechdel %>% 
+  mutate(pass_bechdel = as.integer(if_else(pass_bechdel == "yes", 1, 0)))
+
+glm(pass_bechdel ~ series + issue + writer + issue:writer, data = dat, family = "binomial") %>% 
+  broom::tidy()
+
+#########
+
+comic_bechdel %>% 
+  group_by(series) %>% 
+  summarize(n_writ = n_distinct(writer))
+
+comic_bechdel %>% 
+  count(series, writer, sort = TRUE) %>% 
+  ggplot(aes(series, writer, fill = n)) +
+  geom_tile()
+
